50000f783dfe6e0c1c4eefcd39aad0ec853bd09e
DiogoRTedesco <tedescodiogo@yahoo.com.br>
2024-06-28 16:28:42 -0300

    criação de modulos e services

--- authenticator_api/src/app.module.ts
+++ authenticator_api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { MoService } from './permissions/mo/mo.service';
+import { PermissionsService } from './permissions/permissions.service';
+import { PermissionsModule } from './permissions/permissions.module';
+import { SystemsService } from './systems/systems.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PermissionsModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, MoService, PermissionsService, SystemsService],
 })
 export class AppModule {}
